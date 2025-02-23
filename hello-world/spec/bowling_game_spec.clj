2b32bfc2e0682201e8ed033079b8653c0aed5191
Ichinose Yoshihiro <maruisansmai@gmail.com>
2025-02-23 15:01:29 +0900

    Implement bowling game scoring with frame handling and multiple test cases

--- hello-world/spec/bowling_game_spec.clj
+++ hello-world/spec/bowling_game_spec.clj
@@ -4,4 +4,12 @@
 
 (describe "bowling game"
           (it "should be a gutter game"
-              (should= 0 (score []))))
+              (should= 0 (score (repeat 20 0))))
+          (it "is 20"
+              (should= 20 (score (repeat 20 1))))
+          (it "is one spare"
+              (should= 24 (score (concat [5 5 7] (repeat 17 0)))))
+          (it "is one strike"
+              (should= 20 (score (concat [10 2 3] (repeat 16 0)))))
+          (it "is a perfect game"
+              (should= 300 (score (repeat 12 10)))))
--- hello-world/src/bowling_game.clj
+++ hello-world/src/bowling_game.clj
@@ -1,4 +1,26 @@
 (ns bowling-game)
 
+(defn to-frames [rolls]
+  (loop [remaining-rolls rolls
+         frames []]
+    (cond
+      (empty? remaining-rolls)
+      frames
+
+      (= 10 (first remaining-rolls))
+      (recur (rest remaining-rolls)
+             (conj frames (take 3 remaining-rolls)))
+
+      (= 10 (reduce + (take 2 remaining-rolls)))
+      (recur (drop 2 remaining-rolls)
+             (conj frames (take 3 remaining-rolls)))
+
+      :else
+      (recur (drop 2 remaining-rolls)
+             (conj frames (take 2 remaining-rolls))))))
+
+(defn add-frame [score frame]
+  (+ score (reduce + frame)))
+
 (defn score [rolls]
-  0)
+  (reduce add-frame 0 (take 10 (to-frames rolls))))
